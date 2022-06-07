16b159df7b92379553865d74624e077107788011
Vedant Puri <vedantpuri@gmail.com>
2022-06-07 13:15:40 -0400

    we have everything we need to get started

--- src/Domains.jl
+++ src/Domains.jl
@@ -3,15 +3,26 @@ module Domains
 """ D-Dimensional domain """
 abstract type AbstractDomain{T,D} end
 
+import UnPack: @unpack
+import Setfield: @set!
+
+# overload
+import Base: *, length
+import Base: summary, display
+
 # wrap
 using DomainSets
+using IntervalSets
 
-import Base: *
+# ref
+# https://github.com/SciML/ModelingToolkit.jl/blob/master/src/domains.jl
+# https://github.com/JuliaSymbolics/Symbolics.jl/blob/master/src/domains.jl
 
 include("common.jl")
 include("interval.jl")
 include("box.jl")
 include("deform.jl")
 #include("map.jl")
+#include("jac_map.jl")
 
 end # module Domains
